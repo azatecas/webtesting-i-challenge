fce9362cb68c200aabb07b5ad80731992fdd24a8
Luis PM <luisp237@gmail.com>
2020-03-26 16:33:45 -0400

    refactor function into switch case statement, not working

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,44 +6,55 @@ module.exports = {
 };
 
 function succeed(item) {
-  if (item.enhancement < 20) {
-    const newItem = {
-      name: item.name,
-      durability: 100,
-      enhancement: item.enhancement + 1
-    };
-    return newItem;
+  if (item.enhancement < 20) {    
+     return {...item,enhancement: item.enhancement + 1 };
   } else {
     return item;
   }
 }
 
 function fail(item) {
-  if(item.enhancement < 15){
-    const newItem = {
-      name: item.name,
-      durability: item.durability - 5,
-      enhancement: item.enhancement
-    };
-    return newItem;
-  } else if (item.enhancement > 16){
-    const newItem = {
-      name: item.name,
-      durability: item.durability,
-      enhancement: item.enhancement - 1
-    };
-    return newItem
-  } else if (item.enhancement >= 15){
-    const newItem = {
-      name: item.name,
-      durability: item.durability - 10,
-      enhancement: item.enhancement
-    };
-    return newItem
-  } else {
-    return item
+
+  switch (item) {
+    case item.enhancement < 15:      
+      return {...item, durability: item.durability - 5 }
+    case item.enhancement > 16:
+      return {...item, enhancement: item.enhancement - 1 }
+    case item.enhancement >= 15:
+      return {...item, durability: item.durability - 10 }  
+    default:
+      return item;
   }
+
+  return item;
+
+
+
+  // if(item.enhancement < 15){
+  //   const newItem = {
+  //     ...item,
+  //     durability: item.durability - 5,      
+  //   };
+  //   return newItem;
+  // } else if (item.enhancement > 16){
+  //   const newItem = {
+  //     ...item,
+  //     enhancement: item.enhancement - 1
+  //   };
+  //   return newItem
+  // } else if (item.enhancement >= 15){
+  //   const newItem = {
+  //     ...item,
+  //     durability: item.durability - 10,      
+  //   };
+  //   return newItem
+  // } else {
+  //   return item
+  // }
 }
+
+
+
 
 function repair(item) {
   const newItem = {
